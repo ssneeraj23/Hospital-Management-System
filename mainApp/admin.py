dc46bdfc5a60f2172e73ce2f3d21809af9f0b3e4
Kartik Pontula <kartikpontula@gmail.com>
2023-03-02 19:21:12 +0530

    Added a functional but ugly dashboard.

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Patient)
+admin.site.register(Room)
+admin.site.register(Doctor)
+admin.site.register(Appointment)
+admin.site.register(Admission)
+admin.site.register(Test)
+admin.site.register(Operation)
