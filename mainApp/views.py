1d197d1b0ba09e3f9735b6ef20a4c682a33def7b
Kartik Pontula <kartikpontula@gmail.com>
2023-03-02 19:26:39 +0530

    Implemented a rickroll(?)

--- mainApp/views.py
+++ mainApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def dashboard(request):
-    return HttpResponse("Welcome to this ugly website.")
+    return HttpResponse("<h1>Welcome to this ugly website.</h1><br>Have a <a href=\"https://www.youtube.com/watch?v=izGwDsrQ1eQ\">rickroll</a>.")
