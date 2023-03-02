c4f0ce56f3578fe7c3e70e411eb4c153c4b9d3b4
Kartik Pontula <kartikpontula@gmail.com>
2023-03-02 18:09:30 +0530

    Corrected models.py, makemigrations successful

--- mainApp/models.py
+++ mainApp/models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Patient(models.Model):
-    name = models.CharField(max_length=100, default="Joseph")
+    name = models.CharField(max_length=100, default="John Doe")
     dob = models.DateField(null=True)
     gender = models.CharField(max_length=10, null=True)
     address = models.CharField(max_length=100, null=True)
@@ -15,7 +15,7 @@ class Patient(models.Model):
 
 class Room(models.Model):
     roomType = [('w', "Ward"), ('o', 'Operation Theatre')]
-    type = models.CharField(choices=roomType, default='w')
+    type = models.CharField(max_length=10, choices=roomType, default='w')
     available = models.BooleanField(default=False)
 
 class Doctor(models.Model):
@@ -24,7 +24,7 @@ class Doctor(models.Model):
     phoneNumber = models.CharField(max_length=20, null=True)
     email = models.EmailField(null=True)
     department = models.CharField(max_length = 20)
-    officeNumber = models.CharField(validators=[RegexValidator(r'^\d{1,5}', message="Office number should ")])
+    officeNumber = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,5}', message="Office number should ")])
     def __str__(self):
         return self.name
 
@@ -39,25 +39,25 @@ class Appointment(models.Model):
 
 class Admission(models.Model):
     patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_admitted')
-    roomID = models.ForeignKey(Room, on_delete=models.SET_NULL, related_name='patient_admitted')
+    roomID = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='patient_admitted')
     startTime = models.DateTimeField()
     endTime = models.DateTimeField(null=True, blank=True)
 
-class Test:
+class Test(models.Model):
     testName = models.CharField(max_length=50)
-    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_admitted')
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_tested')
     scheduledTime = models.DateTimeField()
     testReport = models.FileField()
     reportGenerationTime = models.DateTimeField()
 
-class Operation:
+class Operation(models.Model):
     def _validate_room(value):
         if value.type != 'o':
             raise ValidationError('This room is not suitable for operations.')
     opName = models.CharField(max_length=50)
-    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_admitted')
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_operated')
     doctorID = models.ManyToManyField(Doctor, related_name='doctors')
-    opTheatre = models.ForeignKey(Room, related_name='where', validators=[_validate_room])
+    opTheatre = models.ForeignKey(Room, related_name='where', validators=[_validate_room], on_delete=models.CASCADE)
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
     operationReport = models.FileField()
