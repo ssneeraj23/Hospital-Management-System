58cdb8f41482fb72d7777a6653514295337a06b6
gheamanthrao <100954923+gheamanthrao@users.noreply.github.com>
2023-03-03 22:34:26 +0530

    Added some templates

---
+++ doctor/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('doctor',views.doctor, name = 'doctor'),
+    path('pl',views.pl,name = 'pl'),
+    path('pi',views.pi,name = 'pi'),
+    path('ri',views.ri,name = 'ri')
+]
