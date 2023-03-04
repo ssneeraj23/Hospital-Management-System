58159806ad6ac137100f63b36b157fdf33cfa180
Kartik Pontula <kartikpontula@gmail.com>
2023-03-04 15:09:16 +0530

    Imported models and forms from main

--- mainApp/models.py
+++ mainApp/models.py
@@ -11,7 +11,7 @@ class Patient(models.Model):
     phoneNumber = models.CharField(max_length=20, null=True)
     email = models.EmailField(null=True)
     def __str__(self):
-        return self.name
+        return self.pk + " " + self.name
 
 class Room(models.Model):
     roomType = [('w', "Ward"), ('o', 'Operation Theatre')]
@@ -26,22 +26,29 @@ class Doctor(models.Model):
     department = models.CharField(max_length = 20)
     officeNumber = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,5}', message="Office number should be between 1 and 5 digits")])
     def __str__(self):
-        return self.name
+        return self.pk + " " + self.name
 
 class Appointment(models.Model):
     patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_booked')
     doctorID = models.ForeignKey(Doctor, related_name='doctor_assigned', on_delete=models.CASCADE)
     startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
-    prescription = models.TextField()
-    priority = models.IntegerField()
-    appReport = models.FileField()
-    reportGenerationTime = models.DateTimeField()
+    endTime = models.DateTimeField(null=True, blank=True)
+    prescription = models.TextField(null=True, blank=True)
+    priorityChoices = [
+        (0, "Low"),
+        (1, "Medium"),
+        (2, "High")
+    ]
+    priority = models.IntegerField(choices=priorityChoices, default=0)
+    appReport = models.FileField(null=True, blank=True)
+    reportGenerationTime = models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return str(self.pk) + " - " + str(self.patientID) + " - " + self.startTime
 
 class Admission(models.Model):
     def _validate_room(value):
         if value.type != 'w':
-            raise ValidationError('This room is not suitable for operations.')
+            raise ValidationError('This room is not an admission ward')
     patientID = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_admitted')
     roomID = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='patient_admitted', validators=[_validate_room])
     startTime = models.DateTimeField()
