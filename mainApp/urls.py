5761932da26c4a93992f85cf34101e59d0ff4336
Kartik Pontula <kartikpontula@gmail.com>
2023-03-05 19:08:10 +0530

    Pre-rebase commit

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,17 +6,11 @@ from .forms import *
 
 urlpatterns = [
 
-    path('', LoginView.as_view(template_name='login.html', next_page='doctor', authentication_form=LoginForm), name='login'),
-    path('base', views.base, name='base'),
-    path('dataEntry/', views.dataEntry, name='dataEntry'),
-    path('frontDesk/', views.frontDesk, name='frontDesk'),
-    path('doctor/', views.doctor, name='doctor'),
-    # path('doctor',views.doctor, name = 'doctor'),
-    path('patientReg/', views.patientReg, name='patientReg'),
-    path('patientList',views.patientList, name = 'patientList'),
-    path('patientInfo/',views.patientInfo, name = 'patientInfo'),
-    # path('ri',views.ri, name = 'ri'),
-    path('logout', views.logout_view, name = 'logout'),
-    # path('password_reset', views.password_reset, name = 'password_reset'), # TODO
-    # path('access-denied', views.access_denied, name='access-denied') # TODO
+    path('', LoginView.as_view(template_name='login.html',
+         next_page='doctor', authentication_form=LoginForm), name='login'),
+    path('doctorDash', views.docDashView, name='doctorDash'),
+    path('patientList', views.patientListView, name='patientList'),
+    path('patientInfo', views.patientInfoView, name='patientInfo'),
+    path('logout', views.logout_view, name='logout'),
+    path('access-denied', views.access_denied, name='access-denied')  # TODO
 ]
